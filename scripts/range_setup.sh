d7e95b0e8666f6a500977ca6790a6e16db3315c1
Aniket Jivani <ajivani@umich.edu>
2022-12-15 12:14:51 -0600

    correct NRUNS calculation and JETDIR path

--- scripts/range_setup.sh
+++ scripts/range_setup.sh
@@ -1,40 +1,34 @@
 #!/bin/bash                                                                                   
 
-JETDIR=/home/e734/e734/shared/JetSimsMFAL
+JETDIR=/work/e734/e734/shared/ajivani/JetSimsMFAL
 model=$3 # can be HF or LF!                                                                   
 batch=$4
 
 mkdir -p ../runs_${model}_batch_${4}
 export RUN_FOLDER=`readlink -f ../runs_${model}_batch_${4}`
-export NRUNS=`echo $2 - $1 | bc`
+export NRUNS=`echo $2-$1+1 | bc`
 
-cd ../runs_${model}
+cd $RUN_FOLDER
 
 for rID in $(seq -f "%03g" $1 $2); do
     # Copies run template folder.
     mkdir run${rID}
-    cp ../template_run/inlet.dat run${rID}/inlet.dat
+    cd run${rID}
+    cp ../../template_run/inlet.dat ./inlet.dat
     if [ "$model" == "LF" ]
     then
-       cp ../template_run/LoFi_RoundJetG4H.cfg run${rID}/RoundJet_G4H.cfg
+       cp ../../template_run/LoFi_RoundJetG4H.cfg ./RoundJet_G4H.cfg
     else
-	cp ../template_run/HiFi_RoundJetG4H.cfg run${rID}/RoundJet_G4H.cfg
+       cp ../../template_run/HiFi_RoundJetG4H.cfg ./RoundJet_G4H.cfg
     fi
 
-    julia -e "include("./get_input_list.jl")"
+    julia ../../scripts/get_input_list.jl
     
-    cd run${rID}
+    # cd run${rID}
 
     export RUN_ID=${rID}
-    julia -e "include("./modify_config.jl")"
+    julia ../../scripts/modify_config.jl
     
-    # julia -e "include("./range_setup.jl")
-    # doModify flag - if doModify is true then only things are rewritten
-
-    # if doModify
-    # Modifies the correct run input values.                                                  
-    # sh $JETDIR/scripts/modify_inputs.sh ${rID} ${RUNDIR} # check what args need to be given
-    # else do nothing
     
     cd ../
 done
