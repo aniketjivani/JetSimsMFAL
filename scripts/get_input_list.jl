1f4e5fb3c6ccef402e679c563c81be205fe29c5e
Aniket Jivani <ajivani@umich.edu>
2022-12-28 16:10:02 -0800

    give upper and lower bounds for log MEVR directly

--- scripts/get_input_list.jl
+++ scripts/get_input_list.jl
@@ -38,9 +38,9 @@ rhoInf = 0.0722618 # kg / m^3
 RUN_FOLDER = ENV["RUN_FOLDER"]
 NRUNS = parse(Int, ENV["NRUNS"])
 
-# set lower and upper bounds for uncertain parameters (the last value in each is for MEVR and NOT nu_tilde, that is backed out of samples for MEVR at the end!!)
-lower_bounds = [293.24, 0.1, 4.6228]
-upper_bounds = [312.94, 0.3, 100.036]
+# set lower and upper bounds for uncertain parameters (the last value in each is for log MEVR and NOT nu_tilde, that is backed out of samples for log MEVR at the end!!)
+lower_bounds = [293.24, 0.1, 1.531]
+upper_bounds = [312.94, 0.3, 4.6055]
 
 input_list = zeros(NRUNS, 7) # first column is just run ID, next 3 columns are uniform random betn -1 and +1, last 3 are scaled samples in actual units!
 
@@ -53,7 +53,7 @@ input_list[:, 4] = rand(Uniform(-1, 1), NRUNS)
 # scale back to original quantities using lower and upper bounds
 input_list[:, 5] = (1/2) * (upper_bounds[1] - lower_bounds[1]) * input_list[:, 2] .+ (1/2) * (lower_bounds[1] + upper_bounds[1])
 input_list[:, 6] = (1/2) * (upper_bounds[2] - lower_bounds[2]) * input_list[:, 3] .+ (1/2) * (upper_bounds[2] + lower_bounds[2])
-logMEVR = (1/2) * (log(upper_bounds[3]) -log(lower_bounds[3])) * input_list[:, 4] .+ (1/2) * (log(lower_bounds[3]) + log(upper_bounds[3]))
+logMEVR = (1/2) * (upper_bounds[3] -lower_bounds[3]) * input_list[:, 4] .+ (1/2) * (lower_bounds[3] + upper_bounds[3])
 input_list[:, 7] = exp.(logMEVR) * (mu_l / rhoInf) # change logMEVR to nu_tilde
 
 # write to file
