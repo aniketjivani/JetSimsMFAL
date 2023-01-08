c7230372b1f07e461cc01bbd48f280134760d66f
Aniket Jivani <ajivani@umich.edu>
2023-01-08 02:23:18 +0000

    change range run to submit test LF runs

--- scripts/range_run.sh
+++ scripts/range_run.sh
@@ -7,21 +7,14 @@ export RUN_FOLDER=`readlink -f ../runs_${model}_batch_${4}`
 
 cd $RUN_FOLDER
 
-for rID in $(seq $1 $2); do
+for rID in $(seq -f "%03g" $1 $2); do
     start=$(date +%s.%N)
     cd run${rID}
-
-    cp ../../template_run/run_job.sbat ./run_job.sbat
+    pwd
+    cp ../../template_run/run_job.sbat ./
 
     sbatch run_job.sbat
     
     cd ../
-    duration=$(echo "$(date +%s.%N) - $start" |bc)
-    execution_time=`printf "%.5f seconds " $duration`
-    file_out=./output.out
-    if [ ! -f "$file_out" ] ;
-    then
-        touch $file_out
-    fi
-    echo "Run ${rID} completed in ${execution_time}" >&1 | tee -a $file_out
+
 done
